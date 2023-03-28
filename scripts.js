1d39f4cc1ad636700d408f61fcdbecf7b410d45d
Leonardokborges <kborges.leonardo@gmail.com>
2023-03-27 21:09:23 -0300

    É o meu primeiro comit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+function trocacor(cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocaimagem(imagem) {
+    let imgiphone = document.querySelector(".iphone")
+    imgiphone.src= imagem
+    
+}
+   
